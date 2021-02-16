1868bc42c6bf12b773f5f65b4f1271fda15c8884
João_Pedro <joaopedrocz12345@gmail.com>
2021-02-16 01:05:40 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from 'react-native';
+// import firebase from './src/services/firebaseConnection';
+
+import Routes from './src/routes';
+
+const App = () => {
+ return (
+  <NavigationContainer>
+     <StatusBar backgroundColor="#131313" barStyle="light-content"/>
+     <Routes/>
+  </NavigationContainer>
+  );
+}
+
+export default App;
